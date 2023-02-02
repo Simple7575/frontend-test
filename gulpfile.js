cbb6944fe9e80c63a3dfe2f5f4d5009231877ca8
Simple <varg616@gmail.com>
2023-02-02 14:30:02 +0400

    gulp settings finished

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,22 @@
 import gulp from "gulp";
 import { deleteAsync } from "del";
+import gulpZip from "gulp-zip";
 
 import { html } from "./gulp/tasks/htmlTask.js";
 import { scss } from "./gulp/tasks/scssTask.js";
 import { server } from "./gulp/tasks/serverTask.js";
 
-const { series, parallel, watch } = gulp;
+const { src, dest, series, parallel, watch } = gulp;
 const mainTasks = parallel(scss, html);
 
 function clear() {
     return deleteAsync(["dist"]);
+}
+
+export function zip() {
+    return src(["./**", "!./node_modules/**", "!./dist/**"])
+        .pipe(gulpZip("front-militonyan.zip"))
+        .pipe(dest("./"));
 }
 
 export function watcher() {
